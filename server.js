9cdbf758b71839018786b38f73f02fe9c527304d
Francisco Felix <francisco.felix@ext.privalia.com>
2018-12-19 11:27:47 +0100

    Removed unnecesary endpoint from server.js

--- server.js
+++ server.js
@@ -9,15 +9,7 @@ const handle = app.getRequestHandler();
 app.prepare()
   .then(() => {
     const server = express();
-
-    server.get('/a', (req, res) => {
-      return app.render(req, res, '/b', req.query);
-    });
-
-    server.get('/b', (req, res) => {
-      return app.render(req, res, '/a', req.query);
-    });
-
+    
     server.get('/detail/:id', (req, res) => {
       return app.render(req, res, '/detail', { id: req.params.id });
     });
